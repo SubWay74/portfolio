7e3aa626b1369cf98be65fdccdf1a7ca95892bf3
SubWay <sub74741@gmail.com>
2023-01-19 16:11:55 +0200

    adding Receipt saver site to the portfolio

--- js/subs.js
+++ js/subs.js
@@ -4,42 +4,42 @@ const projects = [
     {
         img: "img/restaurant.webp",
         h1: "Restaurant site",
-        discreption: "one of my friends is dreaming about owning a restaurant and that inspire me to make him a very simple site just for a start for his and my future career",
         link: "https://subway74.github.io/Restaurant-site/",
     },
     {
         img: "img/emailTemp.webp",
         h1: "HTML eamil templates",
-        discreption: "Made this HTML-Email-Templates for a client on upwork just for an interveiw",
         link: "https://github.com/SubWay74/HTML-Email-Template",
     },
     {
         img: "img/games.webp",
         h1: "Mini-games",
-        discreption: "I honed JS  skills by building small games to learn more about the logices and it helped me solving some problems ",
         link: "https://github.com/SubWay74?tab=repositories",
+    },
+    {
+        img: "img/receipt-stie.webp",
+        h1: "Receipt stie",
+        link: "https://subway74.github.io/Receipt-Saver",
     },
 ]
 
 for (let i = 0; i < projects.length; i++) {
-    const container = document.createElement("div");
+    const container = document.createElement("a");
     const img = document.createElement("img");
     const h1 = document.createElement("h1");
-    const discreption = document.createElement("p");
-    const button = document.createElement("button");
     const link = document.createElement("a");
+
+    container.setAttribute("href", `${projects[i].link}`);
+    container.setAttribute("target", "_blank");
 
     img.setAttribute("src", projects[i].img);
     h1.innerText = projects[i].h1;
-    discreption.innerText = projects[i].discreption;
-    link.innerText = "Learn More";
     link.setAttribute("href", projects[i].link)
-    button.appendChild(link)
+    container.style.cursor = "pointer";
+    
 
     container.appendChild(img)
     container.appendChild(h1)
-    container.appendChild(discreption)
-    container.appendChild(button)
     projectEl.appendChild(container)
 }
 
